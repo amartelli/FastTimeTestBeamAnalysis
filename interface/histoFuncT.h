99f8af87301a8b37a458bdeab39d63a90890e90c
arabella <arabella.martelli@cern.ch>
2016-05-31 10:50:15 +0200

    template fit

---
+++ interface/histoFuncT.h
@@ -0,0 +1,30 @@
+#ifndef histoFuncT_h
+#define histoFuncT_h
+
+#include "TH1.h"
+#include "TF1.h"
+#include <cstdio>
+
+class histoFuncT
+{
+ public:
+
+
+  //! ctor                                                                                                                                            
+  histoFuncT(TH1F* histo, int& tStart, int& tStop);
+
+  //! dtor                                                                                                                                            
+  ~histoFuncT();
+
+  //! operator()                                                                                                                                      
+  double operator()(double* x, double* par);
+
+
+ private:
+
+  TH1F* histo_p;
+  int tStart_p;
+  int tStop_p;
+};
+
+#endif
